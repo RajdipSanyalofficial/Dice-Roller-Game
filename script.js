536d6c47d66c69cf476372217826d8b2314dd62b
Rajdip-Sanyal <sanyalraj2@gmail.com>
2025-05-16 00:57:18 +0530

    Dice Roller Game Code

---
+++ script.js
@@ -0,0 +1,22 @@
+function RollDice() {
+
+    const numDice = document.getElementById("numberDice").value;
+    const resultDice = document.getElementById("diceResults");
+    const imageDice = document.getElementById("diceImages");
+
+
+    const values=[];
+    const images=[];
+
+    for(let i=0;i<numDice;i++)
+    {
+        const value = Math.floor(Math.random()*6)+1;
+        values.push=(value);
+        images.push(`<img src="dice ${value}.png" alt="Dice ${value}">`)
+    }
+
+    resultDice.textContent=`dice: ${values.join(', ')}`;
+    imageDice.innerHTML = images.join('');
+
+
+}
